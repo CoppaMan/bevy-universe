72d2b084bcdca0aaf7762ace416b58997bbc6cc5
dario <dario.mp@bluewin.ch>
2023-12-12 21:55:15 +0100

    Lock camera to right side up

--- src/objects/camera.rs
+++ src/objects/camera.rs
@@ -13,8 +13,6 @@ use bevy::{
     transform::components::Transform,
 };
 
-use super::components::FocusSphere;
-
 pub struct SpawnCameraPlugin;
 
 impl Plugin for SpawnCameraPlugin {
@@ -29,7 +27,6 @@ struct PanOrbitCamera {
     /// The "focus point" to orbit around. It is automatically updated when panning the camera
     pub focus: Vec3,
     pub radius: f32,
-    pub upside_down: bool,
 }
 
 fn spawn_camera(mut commands: Commands) {
@@ -52,7 +49,6 @@ fn spawn_camera(mut commands: Commands) {
         PanOrbitCamera {
             radius: camera_start_pos.length(),
             focus: Vec3::ZERO,
-            upside_down: false,
         },
     ));
 }
@@ -85,71 +81,42 @@ fn pan_orbit_camera(
 ) {
     // change input mapping for orbit and panning here
     let orbit_button = MouseButton::Right;
-    let pan_button = MouseButton::Middle;
 
-    let mut pan = Vec2::ZERO;
     let mut rotation_move = Vec2::ZERO;
     let mut scroll = 0.0;
-    let mut orbit_button_changed = false;
+
     let win = win_q.get_single().expect("");
 
     if input_mouse.pressed(orbit_button) {
         for ev in ev_motion.read() {
             rotation_move += ev.delta;
         }
-    } else if input_mouse.pressed(pan_button) {
-        // Pan only if we're not rotating at the moment
-        for ev in ev_motion.read() {
-            pan += ev.delta;
-        }
     }
     for ev in ev_scroll.read() {
         scroll += ev.y;
     }
-    if input_mouse.just_released(orbit_button) || input_mouse.just_pressed(orbit_button) {
-        orbit_button_changed = true;
-    }
 
     for (mut pan_orbit, mut transform, _) in query.iter_mut() {
-        if orbit_button_changed {
-            // only check for upside down when orbiting started or ended this frame
-            // if the camera is "upside" down, panning horizontally would be inverted, so invert the input to make it correct
-            let up = transform.rotation * Vec3::Z;
-            pan_orbit.upside_down = up.y <= 0.0;
-        }
+        let up = transform.rotation * Vec3::Z;
 
         let mut any = false;
         if rotation_move.length_squared() > 0.0 {
             any = true;
             let window = get_primary_window_size(win);
+            info!(up.z);
+            let delta_z = rotation_move.x / window.x * std::f32::consts::PI;
             let delta_x = {
-                let delta = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
-                if pan_orbit.upside_down {
-                    -delta
+                let delta = rotation_move.y / window.y * std::f32::consts::PI;
+                if (-0.95 > up.z && delta < 0.0) || (up.z > 0.95 && delta > 0.0) {
+                    0.0
                 } else {
                     delta
                 }
             };
-            let delta_y = rotation_move.y / window.y * std::f32::consts::PI;
-            let yaw = Quat::from_rotation_z(delta_x);
-            let pitch = Quat::from_rotation_x(-delta_y);
-            transform.rotation = yaw * transform.rotation; // rotate around global y axis
+            let roll = Quat::from_rotation_z(-delta_z);
+            let pitch = Quat::from_rotation_x(-delta_x);
+            transform.rotation = roll * transform.rotation; // rotate around global y axis
             transform.rotation = transform.rotation * pitch; // rotate around local x axis
-        } else if pan.length_squared() > 0.0 {
-            /*
-            any = true;
-            // make panning distance independent of resolution and FOV,
-            let window = get_primary_window_size(win);
-            if let Projection::Perspective(projection) = projection {
-                pan *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window;
-            }
-            // translate by local axes
-            let right = transform.rotation * Vec3::X * -pan.x;
-            let up = transform.rotation * Vec3::Y * pan.y;
-            // make panning proportional to distance away from focus point
-            let translation = (right + up) * pan_orbit.radius;
-            pan_orbit.focus += translation;
-            */
         } else if scroll.abs() > 0.0 {
             any = true;
             pan_orbit.radius -= scroll * pan_orbit.radius * 0.2;
